a92164347ffb6b8a0cf9c4e845244e92424ed98a
Thaina da Silva Ebert <thaiebert@gmail.com>
2024-01-21 17:17:59 -0300

    create crud

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,4 +1,6 @@
+import 'package:sqflite/sqflite.dart';
 import 'package:task_list/components/task_card_widget.dart';
+import 'package:task_list/data/database.dart';
 
 //DAO -> Data Access Object
 class TaskDao {
@@ -12,11 +14,48 @@ class TaskDao {
   static const String _difficulty = "difficulty";
   static const String _image = "image";
 
-  save(TaskCardWidget task) async {}
+  save(TaskCardWidget task) async {
+    final Database dataBase = await getDataBase();
+    var itemExists = await getTask(task.taskTitle);
+    if (itemExists.isEmpty) {
+      return await dataBase.insert(_tableName, toMap(task))
+    }
 
-  Future<List<TaskCardWidget>> getAll() async {}
+    return await dataBase.update(_tableName, toMap(task), where: "$_name = ?", whereArgs: [task.taskTitle]);
+  }
 
-  Future<List<TaskCardWidget>> getTask(String taskName) async {}
+  Future<List<TaskCardWidget>> getAll() async {
+    final Database dataBase = await getDataBase();
+    final List<Map<String, dynamic>> result = await dataBase.query(_tableName);
+    return toList(result);
+  }
 
-  delete(String taskName) async {}
+  Future<List<TaskCardWidget>> getTask(String taskName) async {
+    final Database dataBase = await getDataBase();
+    final List<Map<String, dynamic>> result = await dataBase.query(_tableName, where: "$_name = ?", whereArgs: [taskName]);
+    return toList(result);
+  }
+
+  delete(String taskName) async {
+    final Database dataBase = await getDataBase();
+    await dataBase.delete(_tableName, where: "$_name = ?", whereArgs: [taskName]);
+  }
+
+  List<TaskCardWidget> toList(List<Map<String, dynamic>> taskist) {
+    final List<TaskCardWidget> list = [];
+    for (Map<String, dynamic> item in taskist) {
+      final TaskCardWidget task = TaskCardWidget(taskTitle: item[_name], dificult: item[_difficulty], image: item[_image]);
+      list.add(task);
+    }
+    return list;
+  }
+
+  Map<String, dynamic> toMap(TaskCardWidget task) {
+    final Map<String, dynamic> taskMap = {};
+    taskMap[_name] = task.taskTitle;
+    taskMap[_difficulty] = task.dificult;
+    taskMap[_image] = task.image;
+
+    return taskMap;
+  }
 }
